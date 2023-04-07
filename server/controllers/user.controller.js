b8e93d47fd023c0f78c4408f77f5f62a7b64f00a
Mahamodul Hasan Moon <mahamodulhasan.moon@gmail.com>
2023-04-08 02:37:10 +0600

    signup from user done

--- server/controllers/user.controller.js
+++ server/controllers/user.controller.js
@@ -4,10 +4,11 @@ exports.createUser= async(req,res,next) => {
 
     try {
         const user = await signupService(req.body);
+        
         res.status(200).json({
             status: "success",
             message: "Successfully created account",
-            data:user
+            
           });
         
     } catch (error) {
